09a3a40cebdcb031fafe3c1ad71dac1317f9bfe4
Jorite Jean-Marc <o22201909@or-iut-i102-f04.iut45.univ-orleans.fr>
2024-09-03 15:19:09 +0200

    ajout données fichier yml dans une page book

--- tuto/app.py
+++ tuto/app.py
@@ -1,3 +1,8 @@
 from flask import Flask 
+import yaml, os.path
 
 app = Flask(__name__)
+
+data_file_path = os.path.join(os.path.dirname(__file__), 'data/data.yml')
+with open(data_file_path, 'r') as file:
+    data = yaml.safe_load(file)
--- tuto/views.py
+++ tuto/views.py
@@ -1,4 +1,4 @@
-from .app import app
+from .app import app, data
 from flask import render_template
 
 @app.route('/')
@@ -6,3 +6,9 @@ def home():
     return render_template(
         "home.html", title="Hello World", names=["Jean","Paul","Louis"]
     )
+
+@app.route('/books')
+def page_livre():
+    return render_template(
+        "book.html", data=data
+    )   
